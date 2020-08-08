0bd237c7b0fd5d929284495aaa897978370573b0
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-08 13:20:53 -0400

    add new test

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -1,6 +1,7 @@
 const { italic } = require('chalk');
 
 const assert = require('assert');
+const render = require('../../render');
 
 //* Check for input
 it('has a text input', async () => {
@@ -8,4 +9,19 @@ it('has a text input', async () => {
   const input = dom.window.document.querySelector('input');
 
   assert(input);
+});
+
+it('shows a success message with a valid email', async () => {
+  const dom = await render('index.html');
+
+  const input = dom.window.document.querySelector('input');
+  input.value = 'adsfadfasd@sdf.com';
+
+  dom.window.document
+    .querySelector('form')
+    .dispatchEvent(new dom.window.Event('submit'));
+
+  const h1 = dom.window.document.querySelector('h1');
+
+  console.log('Contents of h1', h1.innerHTML);
 });
