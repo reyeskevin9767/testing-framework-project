2eea7916e0a4c4c1f375ed432ad58e1ef5aa4986
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-08 10:26:44 -0400

    determine file or folder and add runtests

--- runner.js
+++ runner.js
@@ -1,14 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+//* Collects Files
 class Runner {
   constructor() {
-    this.files = []
+    this.testFiles = [];
   }
 
-  // Any file that ends with tests.js is added ot an array
-  collecFiles() {
-
+  // Run test files
+  async runTests() {
+    for (let file of this.testFiles) {
+      require(file.name);
+    }
   }
 
+  // Collect all test.js files
+  async collecFiles(targetPath) {
+    const files = await fs.promises.readdir(targetPath);
+
+    // Determine if file or folder
+    for (let file of files) {
+      const filepath = path.join(targetPath, file);
+      const stats = await fs.promises.lstat(filepath);
+
+      // Any file that ends with tests.js is added ot an array
+      if (stats.isFile() && file.includes('.test.js')) {
+        this.testFiles.push({ name: filepath });
+      } else if (stats.isDirectory()) {
+        // Look thorugh folders
+        const childFiles = await fs.promises.readdir(filepath);
+        files.push(...childFiles.map((f) => path.join(file, f)));
+      }
+    }
+  }
 }
-
 
 module.exports = Runner;
