b5b7543fcdf366e8d7e00795a528e1db19c9c9b4
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-08 04:01:46 -0400

    setup runner.js

---
+++ runner.js
@@ -0,0 +1,14 @@
+class Runner {
+  constructor() {
+    this.files = []
+  }
+
+  // Any file that ends with tests.js is added ot an array
+  collecFiles() {
+
+  }
+
+}
+
+
+module.exports = Runner;
